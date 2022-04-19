025cda63afe25799a7704db12dbe1a63b24dd46f
MarkGraebener <mark.w.graebener@gmail.com>
2022-04-19 16:11:52 -0500

    added model

---
+++ src/main/java/com/springblog/springblog/HelloController.java
@@ -0,0 +1,48 @@
+package com.springblog.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class HelloController {
+
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String hello() {
+//        return "This is the landing page!";
+//    }
+
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public int responseAdd(@PathVariable int num1, @PathVariable int num2)
+    {
+        return num1 + num2;
+    }
+    @GetMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public int responseSub(@PathVariable int num1, @PathVariable int num2)
+    {
+        return num1 - num2;
+    }
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public int responseMulti(@PathVariable int num1, @PathVariable int num2)
+    {
+        return num1 * num2;
+    }
+    @GetMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public int responseDivide(@PathVariable int num1, @PathVariable int num2)
+    {
+        return num1 / num2;
+    }
+}
+
---
+++ src/main/java/com/springblog/springblog/PostController.java
@@ -0,0 +1,51 @@
+package com.springblog.springblog;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostController
+{
+    @GetMapping("/")
+    public String hello(@PathVariable Model model)
+    {
+        Posts post1 = new Posts("My First Post", "This is where i will rant");
+        model.addAttribute("post1", post1);
+        return "/Posts/index";
+    };
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String posts()
+    {
+        return "This is the posts page!";
+    };
+
+    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
+
+    public String postsById(@PathVariable String id, Model model)
+    {
+//        Posts post2 = new Posts("My Second Post", "This is where i will rant");
+//        model.addAttribute("post2", post2);
+
+        return  "/Posts/index";
+    };
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    @ResponseBody
+    public String viewPostCreate()
+    {
+
+
+        return  "/Posts/index";
+    };
+
+//    @PostMapping("/posts/create")
+//    @ResponseBody
+//    public String PostCreated()
+//    {
+//        return  " this is to create a new post";
+//    }
+};
